80752f5ddd78f4e812e17f03a8f1957451c14deb
BricePaquette <brice.paquette@gmail.com>
2023-01-20 11:24:25 -0600

    UI tweaks and Bug fixes

---
+++ SAT_Team_Development/SAT_Team_Development.UI.MVC/Models/EnrollmentViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace SAT_Team_Development.UI.MVC.Models
+{
+    public class EnrollmentViewModel
+    {
+        //Property to store Instructor Name and StartDate
+        public string InstructorName { get; set; }
+        public DateTime StartDate { get; set; }
+        public EnrollmentViewModel(string instructorName, DateTime startDate)
+        {
+            InstructorName = instructorName;
+            StartDate = startDate;
+        }
+    }
+}
